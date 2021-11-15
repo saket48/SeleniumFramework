7eb7d49b2f30c3eb39125d28d75e9b792b9c115c
Saket48 <sakethardaha48@gmail.com>
2021-11-15 11:27:45 +0530

    comm 002

--- Framework/src/test/java/com/automation/pages/BaseClass.java
+++ Framework/src/test/java/com/automation/pages/BaseClass.java
@@ -1,13 +1,16 @@
 package com.automation.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 
 import com.automation.utility.BroswerFactory;
 import com.automation.utility.ConfigDataProvider;
 import com.automation.utility.ExcelDataProvider;
+import com.automation.utility.Helper;
 
 public class BaseClass {
 
@@ -32,6 +35,21 @@ public class BaseClass {
 	@AfterClass
 	public void teardown() {
 		BroswerFactory.quitBrowser(driver);
+	}
+	
+	@AfterMethod
+	public void tearDownMethod(ITestResult result) {
+		
+		if(result.getStatus()==ITestResult.FAILURE)
+		{
+			Helper.captureScreenshot(driver);
+			
+		}
+		else if(result.getStatus()==ITestResult.SUCCESS)
+		{
+			Helper.captureScreenshot(driver);
+		}
+		
 	}
 	
 }
---
+++ Framework/src/test/java/com/automation/utility/Helper.java
@@ -0,0 +1,48 @@
+package com.automation.utility;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class Helper {
+
+	
+	public static void captureScreenshot(WebDriver driver) {
+		
+		File src =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		String ScrnshotPath ="./Screenshots/CRM_"+getCurrentDateTime()+"_.png";
+		System.out.println(ScrnshotPath);
+		
+		
+		try {
+			
+			FileHandler.copy(src,new File(ScrnshotPath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Unable to capture Screenshot !!");
+		}
+		
+		
+	}
+
+	public static String getCurrentDateTime() {
+	
+		DateFormat customFormat = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss");
+		Date currDate = new Date();
+			
+		return customFormat.format(currDate);
+	}
+	
+	
+	
+}
+
+	
+	
